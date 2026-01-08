1785e23cbcc61e943b53794d790220024b6c7a0a
Guilherme Cintra <cinguilherme@gmail.com>
2026-01-08 18:11:05 -0300

    ✨ Common producer implemented

--- src/core_service/producers/common.clj
+++ src/core_service/producers/common.clj
@@ -1,16 +1,8 @@
-(ns core-service.producers.producer
+(ns core-service.producers.common
   (:require [integrant.core :as ig]
             [core-service.producers.protocol :as p]))
 
-;; producers-map should be a map of producer-name -> producer-component available in the system map
-;; like {:kafka {:producer :core-service.producers.kafka/producer}
-;;       :rabbitmq {:producer :core-service.producers.rabbitmq/producer}}
-
-;;Or it could be a map of producer-name -> producer instance, but this needs to account that this producers have their life cycles handled by Duct system map and lifecycles.
-
-
 (defrecord CommonProducer [default-producer-key producers]
-  ;; Holds all producers that it can delegate to.
   p/Producer
   (produce! [_ msg-map options]
     (let [producer-key (or (:producer options) default-producer-key)
