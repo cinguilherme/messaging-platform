b4660a0c2f2767c2e45847ad9f5bc219c683dcae
Guilherme Cintra <cinguilherme@gmail.com>
2026-01-09 18:15:49 -0300

    ✨ Storage - minio working state

--- src/core_service/storage/minio.clj
+++ src/core_service/storage/minio.clj
@@ -1,28 +1,65 @@
 (ns core-service.storage.minio
   (:require [integrant.core :as ig]
-            [amazonica.aws.s3 :as s3]
             [duct.logger :as logger]
             [core-service.storage.protocol :as p]))
 
-(defrecord MinioStorage [config bucket logger]
+(import (com.amazonaws.auth AWSStaticCredentialsProvider BasicAWSCredentials)
+        (com.amazonaws.client.builder AwsClientBuilder$EndpointConfiguration)
+        (com.amazonaws.services.s3 AmazonS3 AmazonS3ClientBuilder)
+        (com.amazonaws.services.s3.model ObjectMetadata PutObjectRequest))
+
+(defn- ensure-bucket!
+  [^AmazonS3 client bucket logger]
+  (try
+    (when-not (.doesBucketExistV2 client bucket)
+      (logger/log logger :info ::creating-bucket {:bucket bucket})
+      (.createBucket client bucket))
+    (catch Exception e
+      (logger/log logger :error ::ensure-bucket-failed {:bucket bucket :error (.getMessage e)})
+      (throw e))))
+
+(defn- build-s3-client
+  [{:keys [endpoint access-key secret-key]}]
+  (let [creds (AWSStaticCredentialsProvider.
+               (BasicAWSCredentials. access-key secret-key))
+        ;; MinIO generally expects path-style access for localhost endpoints.
+        builder (doto (AmazonS3ClientBuilder/standard)
+                  (.withCredentials creds)
+                  (.withPathStyleAccessEnabled true)
+                  (.withEndpointConfiguration (AwsClientBuilder$EndpointConfiguration.
+                                              endpoint
+                                              "us-east-1")))]
+    (.build builder)))
+
+(defrecord MinioStorage [^AmazonS3 client bucket logger]
   p/StorageProtocol
   (storage-get [_ key _opts]
     (try
-      (let [result (s3/get-object config :bucket-name bucket :key key)]
-        (slurp (:input-stream result)))
+      (let [obj (.getObject client bucket key)]
+        (with-open [in (.getObjectContent obj)]
+          (slurp in)))
       (catch Exception e
         (logger/log logger :error ::storage-get-failed {:key key :error (.getMessage e)})
         nil)))
   (storage-put [_ key value _opts]
     (try
-      (s3/put-object config :bucket-name bucket :key key :content value)
-      {:ok true}
+      (let [^bytes bytes (.getBytes (str value) "UTF-8")
+            meta (doto (ObjectMetadata.)
+                   (.setContentLength (alength bytes))
+                   (.setContentType "application/json"))
+            req (PutObjectRequest.
+                 bucket
+                 key
+                 (java.io.ByteArrayInputStream. bytes)
+                 meta)]
+        (.putObject client req)
+        {:ok true})
       (catch Exception e
         (logger/log logger :error ::storage-put-failed {:key key :error (.getMessage e)})
         {:ok false :error (.getMessage e)})))
   (storage-delete [_ key _opts]
     (try
-      (s3/delete-object config :bucket-name bucket :key key)
+      (.deleteObject client bucket key)
       {:ok true}
       (catch Exception e
         (logger/log logger :error ::storage-delete-failed {:key key :error (.getMessage e)})
@@ -30,9 +67,9 @@
 
 (defmethod ig/init-key :core-service.storage/minio
   [_ {:keys [endpoint access-key secret-key bucket logger]}]
-  (let [config {:endpoint endpoint
-                :access-key access-key
-                :secret-key secret-key
-                :client-config {:path-style-access-enabled true}}]
-    (logger/log logger :info ::initializing-minio-storage {:endpoint endpoint :bucket bucket})
-    (->MinioStorage config bucket logger)))
+  (logger/log logger :info ::initializing-minio-storage {:endpoint endpoint :bucket bucket})
+  (let [client (build-s3-client {:endpoint endpoint
+                                :access-key access-key
+                                :secret-key secret-key})]
+    (ensure-bucket! client bucket logger)
+    (->MinioStorage client bucket logger)))
