b741bf43d53c89d8c192eadbb31c4341f096a1ec
Guilherme Cintra <cinguilherme@gmail.com>
2026-02-12 15:01:43 -0300

    ✏️ Fix issue with multipart missing on the endpoint for attachment

--- src/core_service/app/server/interceptors.clj
+++ src/core_service/app/server/interceptors.clj
@@ -3,10 +3,11 @@
 
 (defmethod ig/init-key :core-service.app.server.interceptors/http-base-interceptors
   [_ {:keys [metrics cid params response-logger format api-key auth user-context authz
-             json-body coerce-exceptions coerce-request]}]
+             multipart json-body coerce-exceptions coerce-request]}]
   [metrics
    cid
    params
+   multipart
    response-logger
    format
    api-key
