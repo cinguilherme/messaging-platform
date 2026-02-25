e1e5b8903bb25877b40cac6559874e5104f02d57
Guilherme Cintra <cinguilherme@gmail.com>
2026-02-25 08:01:53 -0300

    ♻️ Ditch macro for async message handler

--- src/core_service/app/handlers/handlers.clj
+++ src/core_service/app/handlers/handlers.clj
@@ -5,17 +5,23 @@
 
 (defmethod ig/init-key :core-service.app.handlers.handlers/log-consumed
   [_ {:keys [logger metrics]}]
-  (app-metrics/consumer-handler metrics :log-consumed [envelope]
-                                (let [trace (get-in envelope [:metadata :trace])]
-                                  (logger/log logger :info ::envelope-received {:envelope envelope :trace trace}))))
+  (app-metrics/wrap-consumer
+   metrics
+   :log-consumed
+   (fn [envelope]
+     (let [trace (get-in envelope [:metadata :trace])]
+       (logger/log logger :info ::envelope-received {:envelope envelope :trace trace})))))
 
 (defmethod ig/init-key :core-service.app.handlers.handlers/fail-once
   [_ {:keys [logger metrics]}]
-  (app-metrics/consumer-handler metrics :fail-once [envelope]
-                                (let [trace (get-in envelope [:metadata :trace])]
-                                  (logger/log logger :info ::checking-message {:envelope envelope :trace trace})
-                                  (if (get-in envelope [:msg :original-envelope])
-                                    (logger/log logger :info ::success-on-retry {:envelope envelope :trace trace})
-                                    (do
-                                      (logger/log logger :warn ::failing-first-attempt {:envelope envelope :trace trace})
-                                      (throw (ex-info "Simulated failure on first attempt" {:envelope envelope :trace trace})))))))
+  (app-metrics/wrap-consumer
+   metrics
+   :fail-once
+   (fn [envelope]
+     (let [trace (get-in envelope [:metadata :trace])]
+       (logger/log logger :info ::checking-message {:envelope envelope :trace trace})
+       (if (get-in envelope [:msg :original-envelope])
+         (logger/log logger :info ::success-on-retry {:envelope envelope :trace trace})
+         (do
+           (logger/log logger :warn ::failing-first-attempt {:envelope envelope :trace trace})
+           (throw (ex-info "Simulated failure on first attempt" {:envelope envelope :trace trace}))))))))
